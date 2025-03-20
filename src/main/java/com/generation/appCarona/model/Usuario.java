337eb29ebe61b37cec86479ced4bf55c1a491029
Mina Yura <minammonteiro2@gmail.com>
2025-03-20 10:51:19 -0300

    Usuario controller e login, e algumas configurações de security

--- src/main/java/com/generation/appCarona/model/Usuario.java
+++ src/main/java/com/generation/appCarona/model/Usuario.java
@@ -1,18 +1,9 @@
 package com.generation.appCarona.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
